f98b4fc61747b5e40045321c7e36172c56a2ebf2
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-01 08:01:27 -0400

    Add fixture data and check in Cargo.lock

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate mcpat;
 extern crate sqlite;
-extern crate threed_ice as ice;
 
+use std::fmt::Display;
 use std::fs;
 use std::path::Path;
 
@@ -10,21 +10,36 @@ mod options;
 pub use options::Options;
 
 pub type Result<T> = std::result::Result<T, Error>;
-pub type Error = &'static str;
+pub type Error = Box<Display>;
 
 macro_rules! raise(
     ($message:expr) => (
-        return Err($message);
+        return Err(Box::new($message));
+    );
+);
+
+macro_rules! ok(
+    ($result:expr) => (
+        match $result {
+            Ok(result) => result,
+            Err(error) => raise!(error),
+        }
     );
 );
 
 pub fn process(options: Options) -> Result<()> {
-    match options.config {
-        Some(ref config) => if !exists(config) {
-            raise!("the configuration file does not exist");
-        },
+    let Options { config } = options;
+
+    let config = match config {
+        Some(config) => config,
         None => raise!("a configuration file is required"),
+    };
+    if !exists(&config) {
+        raise!("the configuration file does not exist");
     }
+
+    let _system = ok!(mcpat::open(&config));
+
     Ok(())
 }
 
