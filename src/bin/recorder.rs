24270a3c3e4847f0e40feb88e7e908ab7bf62bf5
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-16 20:11:37 -0400

    Tidy up a bit

--- src/bin/recorder.rs
+++ src/bin/recorder.rs
@@ -10,10 +10,7 @@ mod dynamic;
 mod statik;
 mod support;
 
-use arguments::Arguments;
-use std::{env, process};
-use std::fmt::Display;
-use std::io::{self, Write};
+use recorder::{Error, Result};
 
 const USAGE: &'static str = "
 Usage: recorder <command> [options]
@@ -21,38 +18,31 @@ Usage: recorder <command> [options]
 Commands:
     dynamic        Record the power consumed by a system on a chip.
     static         Record the area occupied by a system on a chip.
-
-Options:
-    --help         Display the usage information of a particular command.
 ";
 
 fn main() {
-    let Arguments { options, orphans, .. } = match arguments::parse(env::args()) {
-        Ok(arguments) => arguments,
-        _ => usage(USAGE),
-    };
-    if orphans.len() != 1 {
+    start().unwrap_or_else(|error| fail(error));
+}
+
+fn start() -> Result<()> {
+    let arguments = ok!(arguments::parse(std::env::args()));
+    if arguments.orphans.len() != 1 {
         usage(USAGE);
     }
-    let result = match &orphans[0][..] {
-        "dynamic" => dynamic::execute(&options),
-        "static" => statik::execute(&options),
-        _ => fail("the command is unknown"),
-    };
-    match result {
-        Err(error) => fail(error),
-        _ => {},
+    match &arguments.orphans[0][..] {
+        "dynamic" => dynamic::execute(&arguments.options),
+        "static" => statik::execute(&arguments.options),
+        _ => raise!("the command is unknown"),
     }
 }
 
-#[allow(unused_must_use)]
-fn fail<T: Display>(error: T) -> ! {
-    io::stderr().write_fmt(format_args!("Error: {}.\n", error));
-    process::exit(1);
+fn fail(error: Error) -> ! {
+    use std::io::{stderr, Write};
+    stderr().write_all(format!("Error: {}.\n", &*error).as_bytes()).unwrap();
+    std::process::exit(1);
 }
 
-#[allow(unused_must_use)]
 fn usage(message: &str) -> ! {
-    io::stderr().write_fmt(format_args!("{}\n", message.trim()));
-    process::exit(0);
+    println!("{}\n", message.trim());
+    std::process::exit(0);
 }
